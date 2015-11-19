434b615af08f75ccca284e0f1dceff4618e081e1
ernestbofill <nest.u333@gmail.com>
2015-11-19 12:16:51 +0100

    review hotel service and xml schema

---
+++ hotelsWS/build/generated/jax-wsCache/service/HotelServiceImplementation/schema/hotelws/HotelListType.java
@@ -0,0 +1,67 @@
+
+package schema.hotelws;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for hotelListType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="hotelListType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence maxOccurs="unbounded" minOccurs="0">
+ *         &lt;element name="hotel" type="{hotelws.schema}hotelType" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "hotelListType", propOrder = {
+    "hotel"
+})
+public class HotelListType {
+
+    protected List<HotelType> hotel;
+
+    /**
+     * Gets the value of the hotel property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the hotel property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getHotel().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link HotelType }
+     * 
+     * 
+     */
+    public List<HotelType> getHotel() {
+        if (hotel == null) {
+            hotel = new ArrayList<HotelType>();
+        }
+        return this.hotel;
+    }
+
+}
